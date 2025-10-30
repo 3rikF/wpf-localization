276a8fd331138a585b169eecd9b080bc8a222793
3rikF <70095154+3rikF@users.noreply.github.com>
2025-10-31 00:05:09 +0100

    Enhanced `MainWindow.xaml`: - Added `TabControl` with localized headers and content templates. - Included resources for implicit localization examples (`ExampleFloatingPointNumber`, `ExampleInteger`, `ExampleDateTimeValue`). - Adjusted layout and added a `ComboBox` for language selection - implemented new `LocalizationBehavior.SyncLanguage`

---
+++ Examples/WpfLocalizationExample/ViewModels/ExampleTabItemViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLocalizationExample.ViewModels;
+
+public sealed class ExampleTabItemViewModel : INotifyPropertyChanged
+{
+	public ExampleTabItemViewModel(string header, string content)
+	{
+		HeaderLangKey = header;
+		ContentLangKey = content;
+	}
+
+	//-----------------------------------------------------------------------------------------------------------------
+	#region Properties
+
+	public string HeaderLangKey
+		{ get; init; } = string.Empty;
+
+	public string ContentLangKey
+		{ get; init; } = string.Empty;
+
+	#endregion Properties
+
+	//-----------------------------------------------------------------------------------------------------------------
+	#region Methods
+
+	public override string ToString()
+		=> $"TabItem: Header='{HeaderLangKey}', Content='{ContentLangKey}'";
+
+	#endregion Methods
+
+	//-----------------------------------------------------------------------------------------------------------------
+	#region INotifyPropertyChanged
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+		=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+	#endregion INotifyPropertyChanged
+}
